ba98c628e4f51cd5ea1667d4db34dc6969a0d74a
roney monte <roneymonte@gmail.com>
2013-11-29 23:27:22 -0200

    codigo C a partir do Netbeans

---
+++ CppApplication_1/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: roney
+ *
+ * Created on November 29, 2013, 11:26 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
